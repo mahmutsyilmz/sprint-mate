dbbbcc8c90629d0443d3cda8ff12c342ae0c8d9b
mahmutsyilmz <mahmut14sami15@gmail.com>
2025-12-29 19:21:17 +0300

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/yilmaz/sprintmate/SprintmateApplication.java
@@ -0,0 +1,13 @@
+package com.yilmaz.sprintmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yilmaz/sprintmate/SprintmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yilmaz.sprintmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
